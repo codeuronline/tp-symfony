809de5e05b7823c9973942a798f3e1bbf9d3095a
codeuronline <j.kasperski@codeur.online>
2022-06-02 14:22:08 +0200

    update

---
+++ projet-symfony/src/Controller/ServiceController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ServiceController extends AbstractController
+{
+    /**
+     * @Route("/service", name="app_service")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ServiceController.php',
+        ]);
+    }
+}
