093ae3cc70ebd9c9b8b508b09f58b613f3a09913
codeuronline <j.kasperski@codeur.online>
2022-06-02 12:52:33 +0200

    update

--- projet-symfony/config/bundles.php
+++ projet-symfony/config/bundles.php
@@ -5,4 +5,7 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
