3621486609710fc960a1d736b46307d6a6656f16
codeuronline <j.kasperski@codeur.online>
2022-06-02 14:29:24 +0200

    update

--- projet-symfony/src/Controller/ServiceController.php
+++ projet-symfony/src/Controller/ServiceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,5 +18,16 @@ class ServiceController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/ServiceController.php',
         ]);
+    }
+     /**
+     * @Route("/create", name="app_service")
+     */
+    public function createAction(): JsonResponse
+    {
+        
+        // return $this->json([
+        //     'message' => 'Welcome to your new controller!',
+        //     'path' => 'src/Controller/ServiceController.php',
+        // ]);
     }
 }
