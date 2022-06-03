457b016029370cced4465ccb24dfd43305e50ad2
codeuronline <j.kasperski@codeur.online>
2022-06-03 14:15:31 +0200

    update

---
+++ projet-symfony2/migrations/Version20220603112013.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220603112013 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE team ADD photo VARCHAR(255) DEFAULT NULL, ADD age INT NOT NULL, ADD adresse VARCHAR(255) NOT NULL, ADD tel VARCHAR(255) NOT NULL, ADD mail VARCHAR(255) NOT NULL, ADD cv VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE team DROP photo, DROP age, DROP adresse, DROP tel, DROP mail, DROP cv');
+    }
+}
--- projet-symfony2/src/Entity/Team.php
+++ projet-symfony2/src/Entity/Team.php
@@ -31,6 +31,41 @@ class Team
      */
     private $positions;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $Photo;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Age;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Adresse;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Tel;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Mail;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $CV;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $superieurhierarchique;
+
     public function __construct()
     {
         $this->positions = new ArrayCollection();
@@ -76,6 +111,90 @@ class Team
         if ($this->positions->removeElement($position)) {
             $position->removeUser($this);
         }
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->Photo;
+    }
+
+    public function setPhoto(?string $Photo): self
+    {
+        $this->Photo = $Photo;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->Age;
+    }
+
+    public function setAge(int $Age): self
+    {
+        $this->Age = $Age;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->Adresse;
+    }
+
+    public function setAdresse(string $Adresse): self
+    {
+        $this->Adresse = $Adresse;
+
+        return $this;
+    }
+
+    public function getTel(): ?string
+    {
+        return $this->Tel;
+    }
+
+    public function setTel(string $Tel): self
+    {
+        $this->Tel = $Tel;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->Mail;
+    }
+
+    public function setMail(string $Mail): self
+    {
+        $this->Mail = $Mail;
+
+        return $this;
+    }
+
+    public function getCV(): ?string
+    {
+        return $this->CV;
+    }
+
+    public function setCV(string $CV): self
+    {
+        $this->CV = $CV;
+
+        return $this;
+    }
+
+    public function getSuperieurhierarchique(): ?string
+    {
+        return $this->superieurhierarchique;
+    }
+
+    public function setSuperieurhierarchique(string $superieurhierarchique): self
+    {
+        $this->superieurhierarchique = $superieurhierarchique;
 
         return $this;
     }
